ab21edc1743a3c1d656b70f28c15be8892b8c906
MJ Brechner <mjbrechner@gmail.com>
2023-01-01 19:46:03 -0500

    added dynamically updating copyright notice

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 document.getElementById("current-date").innerText = `Today is ${formattedDate}.`;
-document.getElementById("place-in-year").innerText = `It is the ${nthDayOfTheYearString} day of the year, so ${percentageElapsed} has elapsed.`;
+
+if (leapYear === false) {
+document.getElementById("place-in-year").innerText = `It is the ${nthDayOfTheYearString} day of the year. ${percentageElapsed} has elapsed.`;
+} else {
+    document.getElementById("place-in-year").innerText = `It is the ${nthDayOfTheYearString} day of the leap year. ${percentageElapsed} has elapsed.`;
+}
+
 document.getElementById("remaining-in-year").innerText = `There are ${daysRemaining} days, or ${percentageRemaining}, remaining in ${year}.`;
 
 
