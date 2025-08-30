1ebd708c0ff99343c4c9a3094be1f1bc6191b9cc
MJ Brechner <mjbrechner@gmail.com>
2025-08-30 15:57:38 -0400

    added functionality for Wins + Losses

--- javascript/baseball.js
+++ javascript/baseball.js
@@ -2,10 +2,15 @@
 
 let inputtedStat;
 let inputtedGamesPlayed;
+let inputtedGamesWon;
+let inputtedGamesLost;
+let inputtedGamesWonAndLost;
 let currentSeasonPace;
 
 const statArea = document.getElementById("stat-input-area");
 const gamesArea = document.getElementById("games-input-area");
+const winsArea = document.getElementById("wins-input-area");
+const lossesArea = document.getElementById("losses-input-area");
 
 
 statArea.addEventListener('input', (event) => {
@@ -13,6 +18,14 @@ statArea.addEventListener('input', (event) => {
 });
 
 gamesArea.addEventListener('input', (event) => {
+    baseballCalculate();
+});
+
+winsArea.addEventListener('input', (event) => {
+    baseballCalculate();
+});
+
+lossesArea.addEventListener('input', (event) => {
     baseballCalculate();
 });
 
@@ -24,17 +37,49 @@ function baseballCalculate() {
 
     inputtedStat = parseFloat(inputtedStat).toFixed(3); // Change to a string as a decimal number literal, keeping the result to two decimal places.
 
-    inputtedGamesPlayed = document.getElementById("games-input-area").value;
+    inputtedGamesPlayed = gamesArea.value;
+    inputtedGamesWon = winsArea.value;
+    inputtedGamesLost = lossesArea.value;
+
+    // Clean up numbers
+
     inputtedGamesPlayed = inputtedGamesPlayed.replace(/,/g, `.`);
     inputtedGamesPlayed = parseFloat(inputtedGamesPlayed).toFixed(0);
 
+    inputtedGamesWon = inputtedGamesWon.replace(/,/g, `.`);
+    inputtedGamesWon = parseFloat(inputtedGamesWon).toFixed(0);
+
+    inputtedGamesLost = inputtedGamesLost.replace(/,/g, `.`);
+    inputtedGamesLost = parseFloat(inputtedGamesLost).toFixed(0);
+
+    if (isNaN(inputtedGamesWon)) {
+        inputtedGamesWon = 0;
+    }
+
+    if (isNaN(inputtedGamesLost)) {
+        inputtedGamesLost = 0;
+    }
+
+    inputtedGamesWonAndLost = (parseInt(inputtedGamesWon) + parseInt(inputtedGamesLost));
+
+    console.log("inputtedGamesWonAndLost: " + inputtedGamesWonAndLost);
+    // Choose value for Games Played depending on whether the user enters a total number or Wins + Losses    
+    if (inputtedGamesPlayed > 0) {
+        // A positive number has been entered into Games Played, so that number should supersede anything in the Wins and Losses sections
+    } else {
+        // Nothing useful has been entered into Games Played, so whatever has been added to Wins + Losses is used instead.
+        inputtedGamesPlayed = inputtedGamesWonAndLost;
+    }
+
+
     currentSeasonPace = ((inputtedStat / inputtedGamesPlayed) * 162).toFixed(3);
 
-
-    if (isNaN(currentSeasonPace)) {
+console.log("THE PACE IS: " + currentSeasonPace);
+    if (isNaN(currentSeasonPace)||(currentSeasonPace > 1000000000)) { // Since using "=== Infinity" doesn't work in strict mode, requiring more than a billion is sufficient for the purposes of pace numbers for a baseball season.
         document.getElementById("baseball-results").innerText = ` `;
     } else {
         document.getElementById("baseball-results").innerText = `Pace is ${currentSeasonPace} for the season.`;
     }
     console.log(`Calculation says the stat is ${inputtedStat} and the Games Played is ${inputtedGamesPlayed}, so the pace is ${currentSeasonPace}.`);
+
 };
