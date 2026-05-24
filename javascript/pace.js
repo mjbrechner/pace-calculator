7dbcb4494ec9387fbac02849ba4247f95d0157b1
MJ Brechner <mjbrechner@gmail.com>
2026-05-24 08:41:57 -0400

    cleaned up redundant code in pace.js

--- javascript/pace.js
+++ javascript/pace.js
@@ -5,18 +5,13 @@ let currentPace;
 
 document.getElementById("number-input-area").addEventListener('input', (event) => {
 
-
     inputtedNumber = document.getElementById("number-input-area").value; // Grab the text added by the user.
 
     inputtedNumber = inputtedNumber.replace(/,/g, `.`); // Change any commas to periods in case the comma is being used as a decimal separator.
 
     inputtedNumber = parseFloat(inputtedNumber).toFixed(2); // Change to a string as a decimal number literal, keeping the result to two decimal places.
 
-    if (leapYear === false) {
-        currentPace = (inputtedNumber / percentageElapsedRaw).toFixed(2);
-    } else {
-        currentPace = (inputtedNumber / percentageElapsedRaw).toFixed(2);
-    }
+    currentPace = (inputtedNumber / percentageElapsedRaw).toFixed(2);
 
     if (isNaN(currentPace)) {
         document.getElementById("pace-results").innerText = ` `;
